eb2d192961c271ea1efe2a33a23af50e6c61a5fb
angelosenpie-art <76907110+angelosenpie-art@users.noreply.github.com>
2021-01-14 01:27:18 +0800

    Update Dashboard.vue

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,9 +1,31 @@
 <template>
-    <app-layout>
+    <app-layout >
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
-            </h2>
+            <nav class="second-menu-bar py-1 border-gray-500 bg-black">
+                <ul class="pl-12 md:flex items-center md:flex-1">
+                    <li>
+                        <a href="#" class="text-white hover:text-green-200 block px-4 py-1 rounded-md text-sm font-bold">Graphics & Design</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-white hover:text-green-200 block px-4 py-1 rounded-md text-sm font-bold">Video & Animation</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-white hover:text-green-200 block px-4 py-1 rounded-md text-sm font-bold">Music & Audio</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-white hover:text-green-200 block px-4 py-1 rounded-md text-sm font-bold">Programming & Tech</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-white hover:text-green-200 block px-4 py-1 rounded-md text-sm font-bold">Entertainment Services</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-white hover:text-green-200 block px-4 py-1 rounded-md text-sm font-bold">Build & Craft Services</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-white hover:text-green-200 block px-4 py-1 rounded-md text-sm font-bold">Skill & Art Services</a>
+                    </li>
+                </ul>
+            </nav>
         </template>
 
         <div class="py-12">
